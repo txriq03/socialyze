974bf4a938c741b48f12b3acc437219856db3d97
Tariq <118397532+txriq03@users.noreply.github.com>
2024-09-24 18:50:43 +0100

    🔨Possible fix for deployment

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import AuthProvider from './context/AuthContext';
+import { AuthProvider } from './context/AuthContext';
 import { QueryProvider } from './lib/react-query/QueryProvider';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
